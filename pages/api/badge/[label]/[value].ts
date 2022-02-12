507cbf1e331604b9f4c74f62ece13d5228bbad85
CodeDoctorDE <codedoctor@linwood.dev>
2022-02-12 18:20:45 +0100

    fix vercel

--- pages/api/badge/[label]/[value].ts
+++ pages/api/badge/[label]/[value].ts
@@ -1,8 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs";
 import ejs from "ejs";
+import { join } from "path";
 
-const template = ejs.compile(fs.readFileSync("./templates/badge/default.svg", {encoding: "utf8"}).toString());
+const template = ejs.compile(fs.readFileSync(join(process.cwd(), "templates", "badge", "default.svg"), {encoding: "utf8"}).toString());
 
 export default function handle(req: NextApiRequest, res: NextApiResponse) {
     const { label, value } = req.query;
