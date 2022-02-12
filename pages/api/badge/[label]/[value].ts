b46d74fdc35327342014ba61e9ddba55e4aa46c5
CodeDoctorDE <codedoctor@linwood.dev>
2022-02-12 18:58:28 +0100

    add dynamic svg sizes

--- pages/api/badge/[label]/[value].ts
+++ pages/api/badge/[label]/[value].ts
@@ -2,13 +2,23 @@ import { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs";
 import ejs from "ejs";
 import { join } from "path";
+import { createCanvas } from "canvas";
 
-const template = ejs.compile(fs.readFileSync(join(process.cwd(), "templates", "badge", "default.svg"), {encoding: "utf8"}).toString());
+const canvas = createCanvas(200, 200)
+const template = ejs.compile(fs.readFileSync(join(process.cwd(), "templates", "badge", "default.svg"), { encoding: "utf8" }).toString());
 
+const ctx = canvas.getContext('2d')
+
+ctx.font = "28px Roboto";
 export default function handle(req: NextApiRequest, res: NextApiResponse) {
     const { label, value } = req.query;
     // Render svg image
+
+
+    const labelWidth = ctx.measureText(label.toString()).width;
+    const valueWidth = ctx.measureText(value.toString()).width;
+
+    const result = template({ label, value, labelWidth, valueWidth });
     res.setHeader("Content-Type", "image/svg+xml");
-    const result = template({label, value});
     res.status(200).end(result);
 }
