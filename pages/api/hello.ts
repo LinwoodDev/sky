256ee1f3453bb9723982efa593acd9b3315993a8
CodeDoctorDE <codedoctor@linwood.dev>
2022-02-13 08:57:09 +0100

    fix hello, fix svg height width total

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default function(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).send("Hello World");
 }
