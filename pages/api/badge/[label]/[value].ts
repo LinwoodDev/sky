4e7157c8c8ac61e858a250c57f9ee99d03e0ff2c
CodeDoctorDE <codedoctor@linwood.dev>
2022-02-13 08:45:15 +0100

    add mono, fix width

--- pages/api/badge/[label]/[value].ts
+++ pages/api/badge/[label]/[value].ts
@@ -2,25 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs";
 import ejs from "ejs";
 import { join } from "path";
-import { createCanvas, GlobalFonts } from "@napi-rs/canvas";
 
-GlobalFonts.registerFromPath(join(process.cwd(), 'fonts', 'Roboto.ttf'), 'Roboto')
-
-const canvas = createCanvas(200, 200)
 const template = ejs.compile(fs.readFileSync(join(process.cwd(), "templates", "badge", "default.svg"), { encoding: "utf8" }).toString());
 
-const ctx = canvas.getContext('2d')
-
-ctx.font = "Roboto 28px";
 export default function handle(req: NextApiRequest, res: NextApiResponse) {
     const { label, value } = req.query;
     // Render svg image
 
-
-    const labelWidth = ctx.measureText(label.toString()).width;
-    const valueWidth = ctx.measureText(value.toString()).width;
-
-    const result = template({ label, value, labelWidth, valueWidth });
+    const result = template({ label, value });
     res.setHeader("Content-Type", "image/svg+xml");
     res.status(200).end(result);
 }
